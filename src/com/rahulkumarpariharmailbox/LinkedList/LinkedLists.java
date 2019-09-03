e59a9f8d6525a0185422cd0b4af4f4911fbae478
Rahul Kumar Parihar <rahulkumarpariharmailbox@gmail.com>
2019-09-03 08:40:28 +0530

    Inital Commit

---
+++ src/com/rahulkumarpariharmailbox/LinkedList/LinkedLists.java
@@ -0,0 +1,15 @@
+package com.rahulkumarpariharmailbox.LinkedList;
+
+/**
+ * Linked Lists Class
+ */
+public class LinkedLists {
+    LinkedLists.Node head;
+
+    static class Node{
+        int data;
+        LinkedLists.Node next;
+
+        Node(int d){data = d; next = null;}
+    }
+}
