2852dd3a55b0c94af9bc45dc54c4562626446c7c
Rahul Kumar Parihar <rahulkumarpariharmailbox@gmail.com>
2019-09-04 10:07:58 +0530

    Adding Execute Method in Main

--- src/com/rahulkumarpariharmailbox/Main.java
+++ src/com/rahulkumarpariharmailbox/Main.java
@@ -1,8 +1,11 @@
 package com.rahulkumarpariharmailbox;
 
+import com.rahulkumarpariharmailbox.BinaryTree.Execute;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Execute exe = new Execute();
+        exe.Run();
     }
 }
