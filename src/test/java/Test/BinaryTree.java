befe4fe74b66f262e4b29239592fa5de040faff9
Rahul Kumar Parihar <rahulkumarpariharmailbox@gmail.com>
2020-03-03 09:39:33 +0530

    Adding test case for insert

--- src/test/java/Test/BinaryTree.java
+++ src/test/java/Test/BinaryTree.java
@@ -1,14 +1,34 @@
 package Test;
 
+import Abstract.BinaryTree.DepthFirstTraversal;
 import Abstract.BinaryTree.Operation;
 import BinaryTree.OperationIterative;
+import BinaryTree.DepthFirstTraversalIterative;
+import Structure.TreeNode;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class BinaryTree {
-    private final Operation operation = new OperationIterative();
+    private final Operation<Integer> operation = new OperationIterative<>();
+    private final DepthFirstTraversal<Integer> traversal = new DepthFirstTraversalIterative<>();
 
     @Test
-    public void CreateTree() {
-
+    public void CreateTreeIterative() {
+        TreeNode<Integer> root = null;
+        int[] nodeData = {1,2,3};
+        boolean leftChild = false;
+        for(int node: nodeData){
+            root = operation.insert(root, node, leftChild);
+            leftChild = !leftChild;
+        }
+        List<Integer> result = traversal.InOrderTraversal(root);
+        List<Integer> requiredResult = new ArrayList<>(Arrays.asList(2,1,3));
+        assertThat(result,is(requiredResult));
     }
 }
